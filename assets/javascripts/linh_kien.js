1e5777d715a0c658dd0914d0e73ec7236fa305f9
phuongngo <ngohoai.phuong@gmail.com>
2019-01-05 19:00:32 +0700

    fix conflict

--- assets/javascripts/linh_kien.js
+++ assets/javascripts/linh_kien.js
@@ -1,5 +1,4 @@
 var linhKienModule = (function($) {
-<<<<<<< HEAD
     return {
         hello: function() {
             console.log("Hello World");
@@ -15,24 +14,3 @@ var linhKienModule = (function($) {
         }
     }    
 })(jQuery);
-=======
-	return {
-		hello: function() {
-			console.log("hello world");
-		},
-		clickEvent: function() {
-			$(".productBox").off("click").on("click", function(event) {
-				console.log("dang chon");
-				if ($(this).hasClass('selected')) {
-				$(this).removeClass("selected");
-
-				} else {
-				$(this).addClass("selected");
-
-				}
-			})
-		}
-	}
-})(jQuery);
-
->>>>>>> 077e8b3a36028d0f36808defa386ec8a35b56440
