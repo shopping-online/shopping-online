ef59c885d9298bb8979964c04e43334b2b40fdb6
Trong Nhan <nhanvtr23@gmail.com>
2019-01-12 21:15:21 +0700

    ejs moi

--- server.js
+++ server.js
@@ -9,7 +9,53 @@ app.use(express.static('src'));
 app.set('views', __dirname + '/src');
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => res.render('index'))
+app.get('/', (req, res) => res.render('index', {hello: ''}));
+app.get('/hello', (req, res) => res.render('index', {hello: 'world'}));
+
+app.get('/card', function(req, res){
+	let cards = [{
+  "Title": 1,
+  "desc": "Basile",
+  "btn": "Crang"
+}, {
+  "Title": 2,
+  "desc": "Fred",
+  "btn": "Seligson"
+}, {
+  "Title": 3,
+  "desc": "Rock",
+  "btn": "Comber"
+}, {
+  "Title": 4,
+  "desc": "Portie",
+  "btn": "Reuble"
+}, {
+  "Title": 5,
+  "desc": "Ginger",
+  "btn": "Plumm"
+}, {
+  "Title": 6,
+  "desc": "Audrye",
+  "btn": "Caulfield"
+}, {
+  "Title": 7,
+  "desc": "Matthias",
+  "btn": "Lillgard"
+}, {
+  "Title": 8,
+  "desc": "Lars",
+  "btn": "Fraschini"
+}, {
+  "Title": 9,
+  "desc": "Jocelyn",
+  "btn": "Birkbeck"
+}, {
+  "Title": 10,
+  "desc": "Julianne",
+  "btn": "Wederell"
+}];
+	res.render('card', {items: cards});
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
