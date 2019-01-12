84a7da3b9888731658db4e61287c315a23176f2a
Trong Nhan <nhanvtr23@gmail.com>
2019-01-12 19:49:34 +0700

    demo_ejs

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8080
+var path = require("path");
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static(__dirname + '/src/assets'));
+app.use(express.static('src'));
+
+app.set('views', __dirname + '/src');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('index'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
