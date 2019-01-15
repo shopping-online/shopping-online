eb9fc60bc70b409c3d2f080155a4b272e31ec680
Trong Nhan <nhanvtr23@gmai.com>
2019-01-15 21:35:43 +0700

    chinh ejs

--- server.js
+++ server.js
@@ -98,7 +98,111 @@ app.get('/card', function (req, res) {
 	res.render('card', {
 		items: cards
 	});
-})
+});
+
+app.get('/devices', function (req, res) {
+	let items =
+	[
+		{
+		  "ten_sp": "Mercedes-Benz",
+		  "gia_km": "2.10",
+		  "gia": "6.37"
+		}, {
+		  "ten_sp": "Mercury",
+		  "gia_km": "3.82",
+		  "gia": "7.89"
+		}, {
+		  "ten_sp": "Mitsubishi",
+		  "gia_km": "2.86",
+		  "gia": "4.63"
+		}, {
+		  "ten_sp": "Smart",
+		  "gia_km": "3.49",
+		  "gia": "5.52"
+		}, {
+		  "ten_sp": "Chevrolet",
+		  "gia_km": "6.04",
+		  "gia": "0.22"
+		}, {
+		  "ten_sp": "Mercury",
+		  "gia_km": "2.01",
+		  "gia": "9.54"
+		}, {
+		  "ten_sp": "Nissan",
+		  "gia_km": "1.79",
+		  "gia": "6.81"
+		}, {
+		  "ten_sp": "Ford",
+		  "gia_km": "1.93",
+		  "gia": "6.74"
+		}, {
+		  "ten_sp": "BMW",
+		  "gia_km": "9.76",
+		  "gia": "1.68"
+		}, {
+		  "ten_sp": "Ford",
+		  "gia_km": "9.13",
+		  "gia": "0.79"
+		}, {
+		  "ten_sp": "Chevrolet",
+		  "gia_km": "5.01",
+		  "gia": "8.71"
+		}, {
+		  "ten_sp": "Infiniti",
+		  "gia_km": "1.62",
+		  "gia": "0.84"
+		}, {
+		  "ten_sp": "Lincoln",
+		  "gia_km": "5.07",
+		  "gia": "7.98"
+		}, {
+		  "ten_sp": "Geo",
+		  "gia_km": "5.65",
+		  "gia": "2.10"
+		}, {
+		  "ten_sp": "Pontiac",
+		  "gia_km": "5.23",
+		  "gia": "5.31"
+		}, {
+		  "ten_sp": "Land Rover",
+		  "gia_km": "8.50",
+		  "gia": "0.13"
+		}, {
+		  "ten_sp": "Ford",
+		  "gia_km": "7.40",
+		  "gia": "5.55"
+		}, {
+		  "ten_sp": "Rolls-Royce",
+		  "gia_km": "0.93",
+		  "gia": "8.87"
+		}, {
+		  "ten_sp": "Honda",
+		  "gia_km": "7.09",
+		  "gia": "9.08"
+		}, {
+		  "ten_sp": "Infiniti",
+		  "gia_km": "6.96",
+		  "gia": "7.25"
+		}, {
+		  "ten_sp": "Mazda",
+		  "gia_km": "3.03",
+		  "gia": "0.77"
+		}, {
+		  "ten_sp": "Mercury",
+		  "gia_km": "6.42",
+		  "gia": "2.33"
+		}, {
+		  "ten_sp": "Infiniti",
+		  "gia_km": "3.80",
+		  "gia": "0.81"
+		}, {
+		  "ten_sp": "Kia",
+		  "gia_km": "9.61",
+		  "gia": "6.34"
+		}
+	];
+	res.render('devices', {jjjj: items});
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
