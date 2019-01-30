ad35da359a81a62eaa86451321823f418e888bf8
Minh Tri <tmtzminhtri@gmail.com>
2019-01-31 04:54:40 +0700

    validation login

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,8 +1,27 @@
+var user = require('../models/users.model');
+
 module.exports.index = (req, res) => {
-	res.render('../views/login');
+	res.render('../views/login',{
+		error :''
+	});
 };
 
 module.exports.postLogin = (req, res) => {
-	let value = req.body;
-	console.log(value)
+	let email = req.body.email;
+	let password = req.body.password;
+	user.findOne({email :email}, function (err, user) {
+		if(!user){
+			res.render('../views/login',{
+				error :'tài khoản không tồn tại'
+			});
+			return;
+		}
+		if(req.body.password !== user.password){
+			res.render('../views/login',{
+				error : 'sai mật khẩu'
+			});
+			return;
+		}
+		res.redirect('/products');
+	});
 }
--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 var _ = require('lodash');
+var bodyParser = require('body-parser')
 
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// connect mongoose
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/Shopping-Online');
+//
 
 var productsRoute = require('./routes/products.route'); 
 var loginRoute = require('./routes/login.route');
@@ -15,6 +22,8 @@ app.set('view engine', 'ejs');
 
 // define static file
 app.use(express.static('public'));
+
+// define route
 app.use('/account',loginRoute);
 app.use('/products',productsRoute);
 
